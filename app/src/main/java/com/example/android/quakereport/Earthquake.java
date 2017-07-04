be2ddc36636430af95fb46d800521c444b43048c
Lucas Run <lucas.run@gmail.com>
2017-07-04 14:14:18 +0200

    final version

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -1,13 +1,33 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.android.quakereport;
 
 /**
- * Created by mhesah on 2017-07-02.
+ * An {@link Earthquake} object contains information related to a single earthquake.
  */
-
 public class Earthquake {
-    private Double mMagnitude;
+
+    /** Magnitude of the earthquake */
+    private double mMagnitude;
+
+    /** Location of the earthquake */
     private String mLocation;
-    private Long mTimeInMilliseconds;
+
+    /** Time of the earthquake */
+    private long mTimeInMilliseconds;
 
     /** Website URL of the earthquake */
     private String mUrl;
@@ -28,15 +48,24 @@ public class Earthquake {
         mUrl = url;
     }
 
-    public Double getMagnitude() {
+    /**
+     * Returns the magnitude of the earthquake.
+     */
+    public double getMagnitude() {
         return mMagnitude;
     }
 
+    /**
+     * Returns the location of the earthquake.
+     */
     public String getLocation() {
         return mLocation;
     }
 
-    public Long getTimeInMilliseconds() {
+    /**
+     * Returns the time of the earthquake.
+     */
+    public long getTimeInMilliseconds() {
         return mTimeInMilliseconds;
     }
 
